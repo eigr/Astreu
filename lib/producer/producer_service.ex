d1b8dcecfb75a94983764f24067bfe00efe5ee01
Adriano Santos <sleipnir@bsd.com.br>
2021-02-03 17:08:23 -0300

    Adjusts type in producer service

--- lib/producer/producer_service.ex
+++ lib/producer/producer_service.ex
@@ -3,17 +3,17 @@ defmodule Astreu.Producer.Service do
   require Logger
   alias GRPC.Server
 
-  @spec publish(Astreu.Protocol.Payload.t(), GRPC.Server.Stream.t()) ::
-          Astreu.Protocol.Payload.t()
+  @spec publish(Astreu.Protocol.Message.t(), GRPC.Server.Stream.t()) ::
+          Astreu.Protocol.Message.t()
   def publish(message_stream, stream) do
     Logger.debug("Received publisher request #{inspect(message_stream)}")
 
     Enum.each(message_stream, fn message ->
       case Astreu.Producer.Dispatcher.dispatch(message) do
         # ACK with success
-        :ok -> Server.send_reply(stream, Astreu.Protocol.Payload.new())
+        :ok -> Server.send_reply(stream, Astreu.Protocol.Message.new())
         # ACK without success
-        _ -> Server.send_reply(stream, Astreu.Protocol.Payload.new())
+        _ -> Server.send_reply(stream, Astreu.Protocol.Message.new())
       end
     end)
   end
