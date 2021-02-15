4f717576e5ffd11237facf6c28af0ceb7f11d0e0
Adriano Santos <sleipnir@bsd.com.br>
2021-02-15 16:20:07 -0300

    Add some validations in producer connections

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,7 +1,7 @@
 # start the current node as a manager
-#:ok = LocalCluster.start()
+# :ok = LocalCluster.start()
 
 # start your application tree manually
-#Application.ensure_all_started(:astreu)
+# Application.ensure_all_started(:astreu)
 
 ExUnit.start()
